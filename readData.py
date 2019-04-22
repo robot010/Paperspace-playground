fd173358053c3703992ab839f21396fb3abcee4a
Bato803 <dengbotaoo@gmail.com>
2019-04-22 09:09:38 -0400

    change path to "/storage/favourita/stores.csv"

--- readData.py
+++ readData.py
@@ -5,7 +5,7 @@ import os
 print(os.getcwd())
 print(os.listdir())
 print(os.listdir("/storage"))
-a = pd.read_csv("/storage/s97lz6k5/favourita/stores.csv")
+a = pd.read_csv("/storage/favourita/stores.csv")
 print(a.head())
 
 
